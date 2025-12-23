b9b3a46847c85d9b30e27c5ac60be525c08d9370
dadang1601 <dadang.suryana1601@gmail.com>
2025-12-23 10:52:46 +0000

    Initial clean Docusaurus setup

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/sdiadmin/sdi-kb/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/sdiadmin/sdi-kb/node_modules/infima/dist/css/default/default.css"),
+  require("/home/sdiadmin/sdi-kb/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/sdiadmin/sdi-kb/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/sdiadmin/sdi-kb/src/css/custom.css"),
+];
